d828e605946678eeb26ca5c48596599ddb4ab022
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-06-02 13:17:57 +0100

    [Refactor] Extract duplicate code in addSlots

--- src/lib/addSlots.js
+++ src/lib/addSlots.js
@@ -1,43 +1,36 @@
 import { compileToFunctions } from 'vue-template-compiler'
 
+function addSlotToVm (vm, slotName, slotValue) {
+  if (Array.isArray(vm.$slots[slotName])) {
+    if (typeof slotValue === 'string') {
+      vm.$slots[slotName].push(vm.$createElement(compileToFunctions(slotValue)))
+    } else {
+      vm.$slots[slotName].push(vm.$createElement(slotValue))
+    }
+  } else {
+    if (typeof slotValue === 'string') {
+      vm.$slots[slotName] = [vm.$createElement(compileToFunctions(slotValue))]
+    } else {
+      vm.$slots[slotName] = [vm.$createElement(slotValue)] // eslint-disable-line no-param-reassign,max-len
+    }
+  }
+}
+
 function addSlots (vm, slots) {
   Object.keys(slots).forEach((key) => {
-    const slotsObj = slots[key]
     if (!(Array.isArray(slots[key])) &&
-        !(slotsObj !== null && typeof slotsObj === 'object') &&
+        !(slots[key] !== null && typeof slots[key] === 'object') &&
         typeof slots[key] !== 'string') {
       throw new Error('slots[key] must be a Component, string or an array of Components')
     }
-    const isArray = Array.isArray(slotsObj)
+    const isArray = Array.isArray(slots[key])
 
     if (isArray) {
-      Object.keys(slotsObj).forEach((objKey) => {
-        if (Array.isArray(vm.$slots[key])) {
-          if (typeof slotsObj[objKey] === 'string') {
-            vm.$slots[key].push(vm.$createElement(compileToFunctions(slotsObj[objKey])))
-          } else {
-            vm.$slots[key].push(vm.$createElement(slotsObj[objKey]))
-          }
-        } else {
-          if (typeof slotsObj[objKey] === 'string') {
-            vm.$slots[key] = [vm.$createElement(compileToFunctions(slotsObj[objKey]))]
-          } else {
-            vm.$slots[key] = [vm.$createElement(slotsObj[objKey])] // eslint-disable-line no-param-reassign,max-len
-          }
-        }
+      slots[key].forEach((slotValue) => {
+        addSlotToVm(vm, key, slotValue)
       })
-    } else if (Array.isArray(vm.$slots[key])) {
-      if (typeof slotsObj === 'string') {
-        vm.$slots[key].push(vm.$createElement(compileToFunctions(slotsObj)))
-      } else {
-        vm.$slots[key].push(vm.$createElement(slotsObj))
-      }
     } else {
-      if (typeof slotsObj === 'string') {
-        vm.$slots[key] = [vm.$createElement(compileToFunctions(slotsObj))]
-      } else {
-        vm.$slots[key] = [vm.$createElement(slotsObj)] // eslint-disable-line no-param-reassign,max-len
-      }
+      addSlotToVm(vm, key, slots[key])
     }
   })
 }
