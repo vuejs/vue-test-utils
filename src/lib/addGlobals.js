563d080e3dc821a02db288c8c699b3d9b802cce4
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-06-05 20:33:12 +0100

    [Test] Add flow to project

--- src/lib/addGlobals.js
+++ src/lib/addGlobals.js
@@ -1,6 +1,8 @@
-export default function addGlobals (globals) {
+// @flow
+
+export default function addGlobals (globals: Object) {
   return {
-    install: function (Vue) {
+    install: function (Vue: Object) {
       Object.keys(globals).forEach(function (key) {
         Vue.prototype[key] = globals[key]
       })
