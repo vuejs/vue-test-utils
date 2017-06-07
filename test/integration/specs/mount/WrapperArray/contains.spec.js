c9cd8223baeba3b97cc89abdfe70c167926ca72b
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-06-07 19:26:18 +0100

    [Tests] At contains spec for WrapperArray

---
+++ test/integration/specs/mount/WrapperArray/contains.spec.js
@@ -0,0 +1,31 @@
+import { compileToFunctions } from 'vue-template-compiler'
+import mount from '../../../../../src/mount'
+
+describe('contains', () => {
+  it('returns true if every Wrapper contains element', () => {
+    const compiled = compileToFunctions('<span><div><p /></div><div><p /></div></span>')
+    const wrapper = mount(compiled)
+    const divArr = wrapper.findAll('div')
+    expect(divArr.contains('p')).to.equal(true)
+  })
+
+  it('returns false if any Wrapper does not contain element', () => {
+    const compiled = compileToFunctions('<div><div></div><div><p /></div></div>')
+    const wrapper = mount(compiled)
+    const divArr = wrapper.findAll('div')
+    expect(divArr.contains('p')).to.equal(false)
+  })
+
+  it('throws an error if selector is not a valid avoriaz selector', () => {
+    const compiled = compileToFunctions('<div><p></p></div>')
+    const wrapper = mount(compiled)
+    const pArr = wrapper.findAll('p')
+    const invalidSelectors = [
+      undefined, null, NaN, 0, 2, true, false, () => {}, {}, { name: undefined }, []
+    ]
+    invalidSelectors.forEach((invalidSelector) => {
+      const message = 'wrapper.contains() must be passed a valid CSS selector or a Vue constructor'
+      expect(() => pArr.contains(invalidSelector)).to.throw(Error, message)
+    })
+  })
+})
