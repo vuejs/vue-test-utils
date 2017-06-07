83b44be2d48a571f1cbdea2670e2f102562aa812
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-06-07 22:25:03 +0100

    [Tests] Add WrapperArray isVueInstance spec

---
+++ test/integration/specs/mount/WrapperArray/isVueInstance.spec.js
@@ -0,0 +1,18 @@
+import mount from '../../../../../src/mount'
+import { compileToFunctions } from 'vue-template-compiler'
+import ComponentWithChildComponent from '../../../../resources/components/component-with-child-component.vue'
+import Component from '../../../../resources/components/component.vue'
+
+describe('isVueInstance', () => {
+  it('returns true if wrapper is Vue instance', () => {
+    const wrapper = mount(ComponentWithChildComponent)
+    expect(wrapper.findAll(Component).isVueInstance()).to.equal(true)
+  })
+
+  it('returns the tag name of the element if it is not a Vue component', () => {
+    const compiled = compileToFunctions('<div><p /></div>')
+    const wrapper = mount(compiled)
+    expect(wrapper.findAll('p').isVueInstance()).to.equal(false)
+  })
+})
+
