40ff8451a83739e926c79cd098d9ca9e70089aa0
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-06-07 22:27:25 +0100

    [Tests] Add WrapperArray name spec

---
+++ test/integration/specs/mount/WrapperArray/name.spec.js
@@ -0,0 +1,14 @@
+/**
+ * Created by eyerburgh on 07/06/2017.
+ */
+import { compileToFunctions } from 'vue-template-compiler'
+import mount from '../../../../../src/mount'
+
+describe('name', () => {
+  it('throws an error when called on a WrapperArray', () => {
+    const compiled = compileToFunctions('<div><div /></div>')
+    const wrapper = mount(compiled)
+    const message = 'name must be called on a single wrapper, use at(i) to access a wrapper'
+    expect(() => wrapper.findAll('div').name()).to.throw(Error, message)
+  })
+})
