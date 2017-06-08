a29b4a033f2995afb5420582be2e22d8730e99c0
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-06-08 12:45:12 +0100

    [Tests] Add VueWrapper update spec

---
+++ test/integration/specs/mount/WrapperArray/update.spec.js
@@ -0,0 +1,16 @@
+import mount from '../../../../../src/mount'
+import ComponentWithVIf from '../../../../resources/components/component-with-v-if.vue'
+
+describe('update', () => {
+  it('causes vm to re render', () => {
+    const TestComponent = {
+      render: h => h(ComponentWithVIf)
+    }
+    const wrapper = mount(TestComponent)
+    expect(wrapper.findAll('.child.ready').length).to.equal(0)
+    const component = wrapper.find(ComponentWithVIf)
+    component.vm.$set(component.vm, 'ready', true)
+    wrapper.findAll(ComponentWithVIf).update()
+    expect(wrapper.findAll('.child.ready').length).to.equal(1)
+  })
+})
