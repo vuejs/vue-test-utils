cb6c59e3fab1b89e6edc6c957ff28527acb2ccb3
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-07-04 08:40:18 +0100

    refactor: explicitly keep some properties of stubbed components

--- src/lib/stub-components.js
+++ src/lib/stub-components.js
@@ -26,7 +26,14 @@ export function stubComponents (component: Component, stubs: Object): void {
         // Remove cached constructor
     delete component.components[stub]._Ctor
     component.components[stub] = {
-      ...component.components[stub],
+      attrs: component.components[stub].attrs,
+      name: component.components[stub].name,
+      on: component.components[stub].on,
+      key: component.components[stub].key,
+      ref: component.components[stub].ref,
+      props: component.components[stub].props,
+      domProps: component.components[stub].domProps,
+      class: component.components[stub].class,
       ...compileToFunctions(stubs[stub])
     }
     Vue.config.ignoredElements.push(stub)
@@ -39,10 +46,18 @@ export function stubAllComponents (component: Component): void {
         // Remove cached constructor
     delete component.components[c]._Ctor
     component.components[c] = {
-      ...component.components[c],
+      attrs: component.components[c].attrs,
+      name: component.components[c].name,
+      on: component.components[c].on,
+      key: component.components[c].key,
+      ref: component.components[c].ref,
+      props: component.components[c].props,
+      domProps: component.components[c].domProps,
+      class: component.components[c].class,
       render: () => {}
     }
     Vue.config.ignoredElements.push(c)
     stubLifeCycleEvents(component.components[c])
   })
 }
+
