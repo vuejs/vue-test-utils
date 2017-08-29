692d2b63051768a36c0f6ac06a19673ee83bd49b
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-08-29 09:20:08 +0100

    test: runs tests on previous vue versions

---
+++ test/test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+test_version_number(){
+  rm -rf node_modules
+  echo "testing vue@$1"
+  npm install
+  npm install vue@$1
+  npm install vue-template-compiler@$1
+  npm test
+}
+
+test_version_number "2.1.10"
+test_version_number "2.2.6"
+test_version_number "2.3.4"
+test_version_number "2.4.2"
