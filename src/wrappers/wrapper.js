9ef35c8d0566314ca9c82eb57ee30ae16e125212
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-09-23 18:37:25 +0100

    fix: return boolean in hasAttribute

--- src/wrappers/wrapper.js
+++ src/wrappers/wrapper.js
@@ -66,7 +66,7 @@ export default class Wrapper implements BaseWrapper {
       throwError('wrapper.hasAttribute() must be passed value as a string')
     }
 
-    return this.element && this.element.getAttribute(attribute) === value
+    return !!(this.element && this.element.getAttribute(attribute) === value)
   }
 
   /**
