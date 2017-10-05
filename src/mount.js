c753e632621ed59e1bffc51a5053c5b5945bac7f
Ankur Kumar <ankurk91@users.noreply.github.com>
2017-10-05 16:18:49 +0530

    fix: link to docs

--- src/mount.js
+++ src/mount.js
@@ -15,7 +15,7 @@ export default function mount (component: Component, options: Options = {}): Vue
     throwError(
       'window is undefined, vue-test-utils needs to be run in a browser environment.\n' +
       'You can run the tests in node using jsdom + jsdom-global.\n' +
-      'See https://vue-test-utils.vuejs.org/en/guides/general-tips.html for more details.'
+      'See https://vue-test-utils.vuejs.org/en/guides/common-tips.html for more details.'
     )
   }
 
