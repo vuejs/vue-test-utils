fd4d7c941cd8539bd2a547dd84683752c01dac05
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-10-12 20:06:19 +0100

    feat(transition): add TransitionGroupStub

---
+++ test/resources/components/component-with-transition-group.vue
@@ -0,0 +1,15 @@
+<template>
+  <transition-group>
+    <div>{{a}}</div>
+    <div></div>
+  </transition-group>
+</template>
+
+<script>
+    export default{
+      name: 'component-with-transition',
+      data: () => ({
+        a: 'a'
+      })
+    }
+</script>
