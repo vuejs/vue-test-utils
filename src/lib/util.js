383c4729dbf419fdf6cad0ffb013dfb86eb8c1e4
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-10-12 19:34:00 +0100

    feat(transitions): add TransitionStub component

--- src/lib/util.js
+++ src/lib/util.js
@@ -3,3 +3,7 @@
 export function throwError (msg: string): void {
   throw new Error(`[vue-test-utils]: ${msg}`)
 }
+
+export function warn (msg: string): void {
+  console.error(`[vue-test-utils]: ${msg}`)
+}
---
+++ test/unit/specs/TransitionStub.spec.js
@@ -0,0 +1,33 @@
+import ComponentWithTransition from '~resources/components/component-with-transition.vue'
+import TransitionStub from '~src/components/TransitionStub'
+import mount from '~src/mount'
+
+describe.only('TransitionStub', () => {
+  it('update synchronously when used as stubs for Transition', () => {
+    const wrapper = mount(ComponentWithTransition, {
+      stubs: {
+        'transition': TransitionStub
+      }
+    })
+    expect(wrapper.text()).contains('a')
+    wrapper.setData({ a: 'b' })
+    expect(wrapper.text()).contains('b')
+  })
+
+  it('logs error when has multiple children', () => {
+    const TestComponent = {
+      template: `
+        <transition><div /><div /></transition>
+      `
+    }
+    const msg = '[vue-test-utils]: <transition> can only be used on a single element. Use <transition-group> for lists.'
+    const error = sinon.stub(console, 'error')
+    mount(TestComponent, {
+      stubs: {
+        'transition': TransitionStub
+      }
+    })
+    expect(error.args[0][0]).to.equal(msg)
+    error.restore()
+  })
+})
