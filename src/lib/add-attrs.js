438603cf166378c3eb8c7776af014ff13792a4ef
re-fort <jeffpko@gmail.com>
2017-10-12 05:08:54 +0000

    fix: restore temporary change to original config after adding attrs/listeners (#87)

--- src/lib/add-attrs.js
+++ src/lib/add-attrs.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 
 export default function addAttrs (vm, attrs) {
+  const originalVueConfig = Vue.config
   Vue.config.silent = true
   if (attrs) {
     vm.$attrs = attrs
   } else {
     vm.$attrs = {}
   }
-  Vue.config.silent = false
+  Vue.config.silent = originalVueConfig.silent
 }
--- src/lib/add-listeners.js
+++ src/lib/add-listeners.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 
 export default function addListeners (vm, listeners) {
+  const originalVueConfig = Vue.config
   Vue.config.silent = true
   if (listeners) {
     vm.$listeners = listeners
   } else {
     vm.$listeners = {}
   }
-  Vue.config.silent = false
+  Vue.config.silent = originalVueConfig.silent
 }
