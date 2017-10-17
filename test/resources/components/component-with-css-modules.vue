9117c5c08bfa77614f78d285c3c0fda3d694a7c3
Nathaniel Blackburn <support@nblackburn.uk>
2017-10-17 11:09:25 +0100

    Added unit test

---
+++ test/resources/components/component-with-css-modules.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="color-red"></div>
+</template>
+
+<style module>
+    .color-red {
+        color: red;
+    }
+</style>
+
+<script>
+    export default{
+      name: 'component-with-css-modules'
+    }
+</script>
