ac48213e93575fc36aa0d2655b61047a8c5650ef
eddyerburgh <edward.yerburgh@gmail.com>
2017-12-04 19:00:21 +0000

    refactor: extract createFunctionalComponent logic

--- src/lib/create-instance.js
+++ src/lib/create-instance.js
@@ -7,49 +7,11 @@ import addListeners from './add-listeners'
 import addProvide from './add-provide'
 import { stubComponents } from './stub-components'
 import { throwError } from './util'
-import cloneDeep from 'lodash/cloneDeep'
 import { compileTemplate } from './compile-template'
 import createLocalVue from '../create-local-vue'
 import extractOptions from '../options/extract-options'
 import deleteMountingOptions from '../options/delete-mounting-options'
-import { compileToFunctions } from 'vue-template-compiler'
-
-function isValidSlot (slot: any): boolean {
-  return Array.isArray(slot) || (slot !== null && typeof slot === 'object') || typeof slot === 'string'
-}
-
-function createFunctionalSlots (slots = {}, h) {
-  if (Array.isArray(slots.default)) {
-    return slots.default.map(h)
-  }
-
-  if (typeof slots.default === 'string') {
-    return [h(compileToFunctions(slots.default))]
-  }
-  const children = []
-  Object.keys(slots).forEach(slotType => {
-    if (Array.isArray(slots[slotType])) {
-      slots[slotType].forEach(slot => {
-        if (!isValidSlot(slot)) {
-          throwError('slots[key] must be a Component, string or an array of Components')
-        }
-        const component = typeof slot === 'string' ? compileToFunctions(slot) : slot
-        const newSlot = h(component)
-        newSlot.data.slot = slotType
-        children.push(newSlot)
-      })
-    } else {
-      if (!isValidSlot(slots[slotType])) {
-        throwError('slots[key] must be a Component, string or an array of Components')
-      }
-      const component = typeof slots[slotType] === 'string' ? compileToFunctions(slots[slotType]) : slots[slotType]
-      const slot = h(component)
-      slot.data.slot = slotType
-      children.push(slot)
-    }
-  })
-  return children
-}
+import createFunctionalComponent from './create-functional-component'
 
 export default function createConstructor (
   component: Component,
@@ -64,20 +26,7 @@ export default function createConstructor (
   }
 
   if (component.functional) {
-    if (mountingOptions.context && typeof mountingOptions.context !== 'object') {
-      throwError('mount.context must be an object')
-    }
-
-    const clonedComponent = cloneDeep(component)
-    component = {
-      render (h) {
-        return h(
-          clonedComponent,
-          mountingOptions.context || component.FunctionalRenderContext,
-          (mountingOptions.context && mountingOptions.context.children && mountingOptions.context.children.map(x => typeof x === 'function' ? x(h) : x)) || createFunctionalSlots(mountingOptions.slots, h)
-        )
-      }
-    }
+    component = createFunctionalComponent(component, mountingOptions)
   } else if (mountingOptions.context) {
     throwError(
       'mount.context can only be used when mounting a functional component'
