764774de4a1775cad3a19690ae3c138eb27d4e2f
eddyerburgh <edward.yerburgh@gmail.com>
2017-12-21 09:25:10 +0000

    test: add test case for mapGetters in setComputed
    closes #176

--- test/unit/specs/mount/Wrapper/setComputed.spec.js
+++ test/unit/specs/mount/Wrapper/setComputed.spec.js
@@ -1,5 +1,6 @@
 import { compileToFunctions } from 'vue-template-compiler'
-import { mount } from '~vue-test-utils'
+import { mount, createLocalVue } from '~vue-test-utils'
+import Vuex, { mapGetters } from 'vuex'
 import ComponentWithComputed from '~resources/components/component-with-computed.vue'
 import ComponentWithWatch from '~resources/components/component-with-watch.vue'
 
@@ -52,6 +53,34 @@ describe('setComputed', () => {
     const wrapper = mount(TestComponent)
     wrapper.setComputed({ b: 3 })
     expect(wrapper.vm.b).to.equal(3)
+  })
+
+  it('works correctly with mapGetters', () => {
+    const localVue = createLocalVue()
+    localVue.use(Vuex)
+    const store = new Vuex.Store({
+      getters: {
+        someGetter: () => false
+      }
+    })
+    const TestComponent = {
+      computed: {
+        ...mapGetters([
+          'someGetter'
+        ]),
+        placeholder () {
+          return this.someGetter
+            ? 'someGetter is true'
+            : 'someGetter is false'
+        }
+      }
+    }
+    const wrapper = mount(TestComponent, {
+      localVue,
+      store
+    })
+    wrapper.setComputed({ someGetter: true })
+    expect(wrapper.vm.placeholder).to.equal('someGetter is true')
   })
 
   it('throws an error if node is not a Vue instance', () => {
