562fd33f5f873d2bb7f85297f3c616c872dcd6ec
eddyerburgh <edward.yerburgh@gmail.com>
2018-01-08 18:50:56 +0000

    fix: compile uncompiled components
    fixes #329

--- test/unit/specs/mount/Wrapper/html.spec.js
+++ test/unit/specs/mount/Wrapper/html.spec.js
@@ -1,12 +1,32 @@
 import { compileToFunctions } from 'vue-template-compiler'
 import { mount } from '~vue-test-utils'
 import Component from '~resources/components/component.vue'
+import ComponentAsAClass from '~resources/components/component-as-a-class.vue'
 
 describe('html', () => {
   it('returns a VueWrappers HTML as a string', () => {
     const expectedHtml = '<div></div>'
     const wrapper = mount(Component)
     expect(wrapper.html()).to.equal(expectedHtml)
+  })
+
+  it('returns a VueWrappers HTML as a string when component has no render function', () => {
+    const wrapper = mount({
+      template: `<div>1<tester></tester></div>`,
+      components: {
+        tester: {
+          template: `<div class="tester">test</div>`
+        }
+      }
+    })
+    const expectedHtml = '<div>1<div class="tester">test</div></div>'
+    wrapper.update()
+    expect(wrapper.html()).to.equal(expectedHtml)
+  })
+
+  it('class component', () => {
+    const wrapper = mount(ComponentAsAClass)
+    expect(wrapper.html()).to.equal('<div></div>')
   })
 
   it('returns a Wrappers HTML as a string', () => {
