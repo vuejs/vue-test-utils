3026fbf0d8182275c1deec5133d6bd8362e09904
eddyerburgh <edward.yerburgh@gmail.com>
2018-01-09 20:13:20 +0000

    chore: add moved warning

---
+++ build/moved-warning.js
@@ -0,0 +1,3 @@
+const warning = '\nVUE-TEST-UILS WARN: vue-test-utils has moved to a scoped package. \nVUE-TEST-UILS WARN: The new repository is available @vue/test-utils \nVUE-TEST-UILS WARN: To install the new package, run npm install --save-dev @vue/test-utils \n'
+
+console.warn(warning)
