6fa6e061dd5035c68419b96fd8b0140aaf6c8a48
Nick Gravelyn <nick@gravelyn.com>
2018-02-12 22:55:34 -0800

    fix: add-attrs and add-listeners correctly restore silent config option (#428)

--- src/lib/add-listeners.js
+++ src/lib/add-listeners.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 
 export default function addListeners (vm, listeners) {
-  const originalVueConfig = Vue.config
+  const originalSilent = Vue.config.silent
   Vue.config.silent = true
   if (listeners) {
     vm.$listeners = listeners
   } else {
     vm.$listeners = {}
   }
-  Vue.config.silent = originalVueConfig.silent
+  Vue.config.silent = originalSilent
 }
