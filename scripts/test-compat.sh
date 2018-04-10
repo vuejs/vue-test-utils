f6956655a13a8515bd8dcdc79f621a933ac80720
Ryo Shibayama <j02521@gmail.com>
2018-04-10 13:43:01 +0900

    Parallelize compatibility tests

--- scripts/test-compat.sh
+++ scripts/test-compat.sh
@@ -2,16 +2,7 @@
 
 set -e
 
-test_version_number(){
-  echo "running unit tests with Vue $1"
-  yarn add -W -D vue@$1 vue-template-compiler@$1 vue-server-renderer@$1
-  yarn test:unit
-  yarn test:unit:karma
-}
-
-test_version_number "2.0.8"
-test_version_number "2.1.10"
-test_version_number "2.2.6"
-test_version_number "2.3.4"
-test_version_number "2.4.2"
-test_version_number "2.5.13"
+echo "running unit tests with Vue $1"
+yarn add -W -D vue@$1 vue-template-compiler@$1 vue-server-renderer@$1
+yarn test:unit
+yarn test:unit:karma
