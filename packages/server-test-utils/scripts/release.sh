5a67f4dc0efd9eb55e72f46b05b81056c3bf59b1
eddyerburgh <edward.yerburgh@gmail.com>
2018-04-13 19:45:51 +0100

    chore: add release script to server-test-utils

---
+++ packages/server-test-utils/scripts/release.sh
@@ -0,0 +1,33 @@
+set -e
+
+if [[ -z $1 ]]; then
+  echo "Enter new version: "
+  read VERSION
+else
+  VERSION=$1
+fi
+
+read -p "Releasing $VERSION - are you sure? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo "Releasing $VERSION ..."
+
+  npm run test
+
+  # build
+  npm run build
+
+  # commit
+  git add -A
+  git add -f \
+    dist/*.js
+  git commit -m "build: server-test-utils $VERSION"
+  npm version $VERSION --message "release: server-test-utils $VERSION"
+
+  # publish
+  git push
+  npm publish
+
+  # generate release note
+  VERSION=$VERSION npm run release:note
+fi
