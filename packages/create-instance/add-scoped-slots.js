73a7e5f5d53401b0e1a29524d31472ae94821799
38elements <mh19820223@gmail.com>
2018-04-14 21:43:27 +0900

    document -> _document

--- packages/create-instance/add-scoped-slots.js
+++ packages/create-instance/add-scoped-slots.js
@@ -10,8 +10,8 @@ export function addScopedSlots (vm: Component, scopedSlots: Object): void {
       throwError('the scopedSlots option does not support a template tag as the root element.')
     }
     const domParser = new window.DOMParser()
-    const document = domParser.parseFromString(template, 'text/html')
+    const _document = domParser.parseFromString(template, 'text/html')
     vm.$_vueTestUtils_scopedSlots[key] = compileToFunctions(template).render
-    vm.$_vueTestUtils_slotScopes[key] = document.body.firstChild.getAttribute('slot-scope')
+    vm.$_vueTestUtils_slotScopes[key] = _document.body.firstChild.getAttribute('slot-scope')
   })
 }
