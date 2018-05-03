7579a2e5ea871d52c45196a7dec951f8549bb665
eddyerburgh <edward.yerburgh@gmail.com>
2018-05-03 21:12:59 +0100

    refactor: rename shallow to shallowMount

--- test/setup/mocha.setup.js
+++ test/setup/mocha.setup.js
@@ -4,6 +4,9 @@ if (process.env.TEST_ENV !== 'node') {
 
 const chai = require('chai')
 const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+
+chai.use(sinonChai)
 
 global.expect = chai.expect
 global.sinon = sinon
