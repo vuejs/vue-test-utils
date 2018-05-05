864eba3f9c715a87c73a34b72cd307706e93bbf3
eddyerburgh <edward.yerburgh@gmail.com>
2018-05-05 21:36:12 +0100

    test: fix flow error

--- packages/create-instance/create-instance.js
+++ packages/create-instance/create-instance.js
@@ -59,6 +59,7 @@ export default function createInstance (
 
   const instanceOptions = { ...options }
   deleteoptions(instanceOptions)
+  // $FlowIgnore  
   const stubComponents = createComponentStubs(component.components, options.stubs)
 
   if (options.stubs) {
