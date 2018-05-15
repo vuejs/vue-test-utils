4473d2e620f6650ca640e7f7605ee8d372a88ba1
briwa <shabriwa.shalat@gmail.com>
2018-05-15 23:26:09 +0800

    fix: clone propsData to avoid mutation

---
+++ test/specs/mounting-options/propsData.spec.js
@@ -0,0 +1,34 @@
+import { shallowMount } from '~vue/test-utils'
+import ComponentWithProps from '~resources/components/component-with-props.vue'
+import { describeIf } from '~resources/utils'
+
+const baseData = {
+  prop1: ['', '']
+}
+
+describeIf(process.env.TEST_ENV !== 'node',
+  'propsData', () => {
+    let wrapper
+
+    beforeEach(() => {
+      wrapper = shallowMount(ComponentWithProps, {
+        propsData : baseData,
+      })
+    })
+
+    afterEach(() => {
+      wrapper = null
+    })
+
+    describe('should not modify propsData between tests', () => {
+      it('should have the correct props after modifying', () => {
+        expect(wrapper.vm.prop1).to.have.length(2)
+        wrapper.setProps({ prop1: [] });
+        expect(wrapper.vm.prop1).to.have.length(0)
+      })
+
+      it('should have the default props despite being modified in the previous test', () => {
+        expect(wrapper.vm.prop1).to.have.length(2)
+      })
+    })
+  })
