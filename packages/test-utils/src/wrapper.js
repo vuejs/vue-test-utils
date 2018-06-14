8cf6f9896fe1ce61df97aeec46fabb53c2e1873d
eddyerburgh <edward.yerburgh@gmail.com>
2018-06-14 19:21:17 +0100

    refactor: remove redundant assignments

--- packages/test-utils/src/wrapper.js
+++ packages/test-utils/src/wrapper.js
@@ -518,26 +518,21 @@ export default class Wrapper implements BaseWrapper {
     if (this.isFunctionalComponent) {
       throwError('wrapper.setProps() cannot be called on a functional component')
     }
-    if (!this.isVueInstance() || !this.vm) {
+    if (!this.isVm) {
       throwError('wrapper.setProps() can only be called on a Vue instance')
     }
-    if (this.vm && this.vm.$options && !this.vm.$options.propsData) {
-      this.vm.$options.propsData = {}
-    }
+
     Object.keys(data).forEach((key) => {
       // Ignore properties that were not specified in the component options
       // $FlowIgnore : Problem with possibly null this.vm
-      if (!this.vm.$options._propKeys || !this.vm.$options._propKeys.some(prop => prop === key)) {
+      if (!this.vm.$options._propKeys ||
+        !this.vm.$options._propKeys.some(prop => prop === key)) {
         throwError(`wrapper.setProps() called with ${key} property which is not defined on component`)
       }
 
       // $FlowIgnore : Problem with possibly null this.vm
       if (this.vm._props) {
         this.vm._props[key] = data[key]
-        // $FlowIgnore : Problem with possibly null this.vm.$props
-        this.vm.$props[key] = data[key]
-        // $FlowIgnore : Problem with possibly null this.vm.$options
-        this.vm.$options.propsData[key] = data[key]
       } else {
         // $FlowIgnore : Problem with possibly null this.vm
         this.vm[key] = data[key]
