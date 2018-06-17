ac07c713971b9ae8d04d730e7defd0b5303fa870
38elements <mh19820223@gmail.com>
2018-06-18 01:24:40 +0900

    check componentInstance

--- packages/test-utils/src/create-wrapper.js
+++ packages/test-utils/src/create-wrapper.js
@@ -7,7 +7,10 @@ import VueWrapper from './vue-wrapper'
 export default function createWrapper (
   node: VNode | Component,
   options: WrapperOptions
-) {
+): VueWrapper | Wrapper {
+  if (node.componentInstance) {
+    return new VueWrapper(node.componentInstance, options)
+  }
   return node instanceof Vue
     ? new VueWrapper(node, options)
     : new Wrapper(node, options)
