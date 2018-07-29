3a7f094e8275ccd9e860fe819855182cbe9ffc77
eddyerburgh <edward.yerburgh@gmail.com>
2018-07-29 06:08:01 +0100

    test: only run test in vue > 2.4

--- test/specs/mount.spec.js
+++ test/specs/mount.spec.js
@@ -162,19 +162,21 @@ describeRunIf(process.env.TEST_ENV !== 'node', 'mount', () => {
     expect(wrapper.html()).to.equal(`<div></div>`)
   })
 
-  it('handles components as dynamic imports', (done) => {
-    const TestComponent = {
-      template: '<div><async-component /></div>',
-      components: {
-        AsyncComponent: () => import('~resources/components/component.vue')
+  itDoNotRunIf(
+    vueVersion < 2.4, // auto resolve of default export added in 2.4
+    'handles components as dynamic imports', (done) => {
+      const TestComponent = {
+        template: '<div><async-component /></div>',
+        components: {
+          AsyncComponent: () => import('~resources/components/component.vue')
+        }
       }
-    }
-    const wrapper = mount(TestComponent)
-    setTimeout(() => {
-      expect(wrapper.find(Component).exists()).to.equal(true)
-      done()
+      const wrapper = mount(TestComponent)
+      setTimeout(() => {
+        expect(wrapper.find(Component).exists()).to.equal(true)
+        done()
+      })
     })
-  })
 
   it('logs if component is extended', () => {
     const msg =
