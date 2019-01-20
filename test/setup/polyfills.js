ade439863ccdff175e0744239debcfd758383540
Edd Yerburgh <edward.yerburgh@gmail.com>
2019-01-20 18:37:33 +0000

    refactor: remove polyfills (#1110)
    BREAKING CHANGE: Remove polyfills from library

--- test/setup/polyfills.js
+++ test/setup/polyfills.js
@@ -6,8 +6,8 @@ if (typeof Element !== 'undefined' && !Element.prototype.matches) {
     Element.prototype.oMatchesSelector ||
     Element.prototype.webkitMatchesSelector ||
     function (s) {
-      const matches = (this.document || this.ownerDocument).querySelectorAll(s)
-      let i = matches.length
+      var matches = (this.document || this.ownerDocument).querySelectorAll(s)
+      var i = matches.length
       while (--i >= 0 && matches.item(i) !== this) {}
       return i > -1
     }
