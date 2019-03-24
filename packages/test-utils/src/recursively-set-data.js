21ff60f94e8cac10e59da2718eb6d341600c3494
briwa <shabriwa.shalat@gmail.com>
2019-03-24 13:18:29 +0800

    refactor: DRY-er

--- packages/test-utils/src/recursively-set-data.js
+++ packages/test-utils/src/recursively-set-data.js
@@ -3,31 +3,29 @@ import { throwError } from 'shared/util'
 
 export function recursivelySetData(vm, target, data) {
   for (const key in data) {
+    let val
+    let targetVal
+
     const dotIndex = key.indexOf('.')
     if (dotIndex === 0) {
       throwError(`Data key cannot start with a period (evaluating '${key}').`)
     }
 
     if (dotIndex < 0) {
-      const val = data[key]
-      const targetVal = target[key]
-
-      if (isPlainObject(val) && isPlainObject(targetVal)) {
-        recursivelySetData(vm, targetVal, val)
-      } else {
-        vm.$set(target, key, val)
-      }
+      val = data[key]
+      targetVal = target[key]
     } else {
       const firstKey = key.slice(0, dotIndex)
       const remainingKey = key.slice(dotIndex + 1, key.length)
-      const val = { [remainingKey]: data[key] }
-      const targetVal = target[firstKey]
 
-      if (isPlainObject(targetVal)) {
-        recursivelySetData(vm, targetVal, val)
-      } else {
-        vm.$set(target, firstKey, data)
-      }
+      val = { [remainingKey]: data[key] }
+      targetVal = target[firstKey]
+    }
+
+    if (isPlainObject(val) && isPlainObject(targetVal)) {
+      recursivelySetData(vm, targetVal, val)
+    } else {
+      vm.$set(target, key, val)
     }
   }
 }
