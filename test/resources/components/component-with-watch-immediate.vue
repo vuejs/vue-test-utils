a313af286a4a162a168a6304528e1855541008f4
Illya Klymov <xanf@xanf.me>
2019-09-02 18:42:01 +0300

    chore: Add async test with immediate watcher

---
+++ test/resources/components/component-with-watch-immediate.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  props: ['prop1'],
+  data: function() {
+    return {
+      data1: null
+    }
+  },
+
+  watch: {
+    prop1: {
+      handler() {
+        this.data1 = this.prop1
+      },
+      immediate: true
+    }
+  }
+}
+</script>
+<template>
+  <h1>test</h1>
+</template>
