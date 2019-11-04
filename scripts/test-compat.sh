581d77c33d3efc36e406e6e6fd6c6ca60e98feb5
Victor van Poppelen <vvanpoppelen@gmail.com>
2019-11-04 14:46:19 -0800

    Update compatibility test versions
    Update minor versions of running `yarn test:compat` to the latest patch
    revisions.
    
    Add optional argument to `yarn test:compat`, to allow running tests
    against arbitrary `vue` versions.
    
    Run compatibility tests in reverse order, to ensure the most relevant
    test failures are displayed.
    
    Run `yarn add` non-interactively, to force errors if `yarn test:compat`
    is passed an invalid value.
    
    Run `yarn add` without clobbering the lockfile.

--- scripts/test-compat.sh
+++ scripts/test-compat.sh
@@ -2,7 +2,24 @@
 
 set -e
 
-echo "running unit tests with Vue $1"
-yarn add -W -D vue@$1 vue-template-compiler@$1 vue-server-renderer@$1
-yarn test:unit
-yarn test:unit:karma
+run() {
+  echo "running unit tests with Vue $1"
+  yarn add --pure-lockfile --non-interactive -W -D "vue@$1" "vue-template-compiler@$1" "vue-server-renderer@$1"
+  yarn test:unit
+  yarn test:unit:karma
+}
+
+if [ "$1" ]; then
+  run "$1"
+  exit
+fi
+
+vue_version=$(yarn -s info vue version)
+vue_major=$(echo "$vue_version" | cut -d. -f1)
+vue_minor=$(echo "$vue_version" | cut -d. -f2)
+
+while [ "$vue_minor" -gt 0 ]; do
+  vue_minor=$((vue_minor - 1))
+
+  run "${vue_major}.${vue_minor}"
+done
