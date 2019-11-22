142fe1d1235a84a1179f1aa4a339471b33bd8f41
Jamie Stumme <jamesstumme@gmail.com>
2019-11-22 06:39:38 -0600

    Add test for injection on composition api component.

---
+++ test/resources/components/component-with-inject-composition.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ fromMount }}
+  </div>
+</template>
+
+<script>
+import { createComponent, reactive, toRefs, onMounted, inject } from '@vue/composition-api';
+
+export default createComponent({
+  name: 'component-with-inject-composition',
+  setup: () => {
+    const fromMount = inject('fromMount');
+    const setInBeforeCreate = 'created';
+
+    return {
+      fromMount,
+      setInBeforeCreate,
+    }
+  }
+});
+</script>
