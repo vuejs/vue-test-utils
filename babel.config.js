d4d3549e8815d1698d86a432b1d214aee20b1d7c
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-06-30 21:25:41 +1000

    chore: add missing deps

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  plugins: ['transform-flow-strip-types', 'transform-vue-jsx'],
+  plugins: [
+    'transform-flow-strip-types',
+    'transform-vue-jsx',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }]
+  ],
   presets: [
     [
       '@babel/preset-env',
