ec0fe231547b9713ea23d7a55b82906f26c95abe
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-07-06 21:14:24 +1000

    test: placeholder to prevent false failure

--- test/specs/render.spec.js
+++ test/specs/render.spec.js
@@ -2,14 +2,17 @@ import { render } from '@vue/server-test-utils'
 import Cheerio from 'cheerio'
 import { describeDoNotRunIf } from 'conditional-specs'
 
-describeDoNotRunIf(process.env.TEST_ENV !== 'node', 'render', () => {
-  it('returns a cheerio wrapper of the rendered component', async () => {
-    const TestComponent = {
-      template: '<div><h2>Test</h2><p></p><p></p></div>'
-    }
-    const wrapper = await render(TestComponent)
-    expect(wrapper).toBeAnInstanceof(Cheerio)
-    expect(wrapper.find('h2').text()).toEqual('Test')
-    expect(wrapper.find('p').length).toEqual(2)
+describe('render', () => {
+  it.todo('placeholder')
+  describeDoNotRunIf(process.env.TEST_ENV !== 'node', 'render', () => {
+    it('returns a cheerio wrapper of the rendered component', async () => {
+      const TestComponent = {
+        template: '<div><h2>Test</h2><p></p><p></p></div>'
+      }
+      const wrapper = await render(TestComponent)
+      expect(wrapper).toBeAnInstanceof(Cheerio)
+      expect(wrapper.find('h2').text()).toEqual('Test')
+      expect(wrapper.find('p').length).toEqual(2)
+    })
   })
 })
--- test/specs/renderToString.spec.js
+++ test/specs/renderToString.spec.js
@@ -3,88 +3,91 @@ import { createLocalVue } from '@vue/test-utils'
 import ComponentWithChild from '~resources/components/component-with-child.vue'
 import { describeDoNotRunIf } from 'conditional-specs'
 
-describeDoNotRunIf(process.env.TEST_ENV !== 'node', 'renderToString', () => {
-  it('returns a promise', async () => {
-    const str = await renderToString({
-      template: `<div>{{this.val}}</div>`,
-      data() {
-        return { val: '123' }
-      }
-    })
-    expect(str).toContain('123')
-  })
-
-  it('mounts functional component with a defined context when no context object passed in options', async () => {
-    const defaultValue = '[vue-test-utils]: testProp default value'
-    const Component = {
-      functional: true,
-      props: {
-        testProp: {
-          type: String,
-          default: defaultValue
+describe('renderToString', () => {
+  it.todo('placeholder')
+  describeDoNotRunIf(process.env.TEST_ENV !== 'node', 'renderToString', () => {
+    it('returns a promise', async () => {
+      const str = await renderToString({
+        template: `<div>{{this.val}}</div>`,
+        data() {
+          return { val: '123' }
         }
-      },
-      render: (h, { props }) => h('div', props.testProp)
-    }
-    const str = await renderToString(Component)
-    expect(str).toContain(defaultValue)
-  })
-
-  it('mounts component using passed localVue as base Vue', async () => {
-    const TestComponent = {
-      template: `<div>{{test}}</div>`
-    }
-    const localVue = createLocalVue()
-    localVue.prototype.test = 'some value'
-    const str = await renderToString(TestComponent, {
-      localVue: localVue
+      })
+      expect(str).toContain('123')
     })
-    expect(str).toContain('some value')
-  })
 
-  it('adds variables to vm when passed', async () => {
-    const TestComponent = {
-      template: `
+    it('mounts functional component with a defined context when no context object passed in options', async () => {
+      const defaultValue = '[vue-test-utils]: testProp default value'
+      const Component = {
+        functional: true,
+        props: {
+          testProp: {
+            type: String,
+            default: defaultValue
+          }
+        },
+        render: (h, { props }) => h('div', props.testProp)
+      }
+      const str = await renderToString(Component)
+      expect(str).toContain(defaultValue)
+    })
+
+    it('mounts component using passed localVue as base Vue', async () => {
+      const TestComponent = {
+        template: `<div>{{test}}</div>`
+      }
+      const localVue = createLocalVue()
+      localVue.prototype.test = 'some value'
+      const str = await renderToString(TestComponent, {
+        localVue: localVue
+      })
+      expect(str).toContain('some value')
+    })
+
+    it('adds variables to vm when passed', async () => {
+      const TestComponent = {
+        template: `
           <div>
             {{$store.store}}
             {{$route.path}}
           </div>
         `
-    }
-    const $store = { store: true }
-    const $route = { path: 'http://test.com' }
-    const str = await renderToString(TestComponent, {
-      mocks: {
-        $store,
-        $route
       }
-    })
-    expect(str).toContain('true')
-    expect(str).toContain('http://test.com')
-  })
-
-  it('mounts component with $parent set to options.parentComponent', async () => {
-    const Parent = {
-      data: () => ({
-        customName: 'Parent Name'
+      const $store = { store: true }
+      const $route = { path: 'http://test.com' }
+      const str = await renderToString(TestComponent, {
+        mocks: {
+          $store,
+          $route
+        }
       })
-    }
-    const TestComponent = {
-      template: '<div>{{$parent.customName}}</div>'
-    }
-    const str = await renderToString(TestComponent, {
-      parentComponent: Parent
+      expect(str).toContain('true')
+      expect(str).toContain('http://test.com')
     })
-    expect(str).toContain('Parent Name')
-  })
 
-  it('replaces component with template string ', async () => {
-    const str = await renderToString(ComponentWithChild, {
-      stubs: {
-        ChildComponent: '<div class="stub"></div>'
+    it('mounts component with $parent set to options.parentComponent', async () => {
+      const Parent = {
+        data: () => ({
+          customName: 'Parent Name'
+        })
       }
+      const TestComponent = {
+        template: '<div>{{$parent.customName}}</div>'
+      }
+      const str = await renderToString(TestComponent, {
+        parentComponent: Parent
+      })
+      expect(str).toContain('Parent Name')
     })
 
-    expect(str).toContain('"stub"')
+    it('replaces component with template string ', async () => {
+      const str = await renderToString(ComponentWithChild, {
+        stubs: {
+          ChildComponent: '<div class="stub"></div>'
+        }
+      })
+
+      expect(str).toContain('"stub"')
+    })
   })
 })
