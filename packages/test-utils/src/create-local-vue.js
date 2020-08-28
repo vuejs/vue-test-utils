8bbc3d15406859e526e977d9bb0e5fed17c27be3
Bill <bglesias@gmail.com>
2020-08-28 01:08:56 -0400

    docs(createlocalvue): document public createLocalVue API

--- packages/test-utils/src/create-local-vue.js
+++ packages/test-utils/src/create-local-vue.js
@@ -2,6 +2,11 @@
 
 import _createLocalVue from 'shared/create-local-vue'
 
+/**
+ * Returns a local vue instance to add components, mixins and install plugins without polluting the global Vue class
+ * @param {VueConfig} config
+ * @returns {Component}
+ */
 function createLocalVue(config: VueConfig = {}): Component {
   return _createLocalVue(undefined, config)
 }
