3849203b93ce5d2229997e0c987f0117d2c521d4
pimlie <pimlie@hotmail.com>
2021-06-08 17:49:26 +0200

    fix #1845: add functional component check in component name match

---
+++ test/resources/components/component-with-name-prop.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p class="prop-name">{{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'component-with-name-prop',
+  props: ['name']
+}
+</script>
