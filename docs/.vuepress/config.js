e142d37e2bf4d957dccd41dbbb9f5ab2e1993b29
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-06-06 11:05:28 +0100

    docs(pt): prepare the ground to upload docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,7 +24,12 @@ module.exports = {
       title: 'Vue Test Utils',
       lang: 'fr',
       description: 'Utilitaires pour tester les composants Vue'
-    }
+    },
+    '/pt/': {
+      title: 'Vue Test Utils',
+      lang: 'pt',
+      description: 'Utilitários para testes de componentes de Vue'
+    },
   },
   plugins: ['@vuepress/pwa'],
   head: [
@@ -203,7 +208,42 @@ module.exports = {
           '/fr/api/options',
           '/fr/api/components/'
         ]
-      }
+      },
+      '/': {
+        label: 'Português',
+        selectText: 'Idiomas',
+        editLinkText: 'Edite esta página no GitHub',
+        nav: [
+          {
+            text: 'API',
+            link: '/pt/api/'
+          },
+          {
+            text: 'Guias',
+            link: '/pt/guides/',
+            items: [
+              {
+                text: '2.x-beta',
+                link: 'https://next.vue-test-utils.vuejs.org/guide/'
+              }
+            ]
+          },
+          {
+            text: 'Atualizando para V1',
+            link: '/pt/upgrading-to-v1/'
+          }
+        ],
+        sidebar: [
+          '/pt/',
+          '/pt/installation/',
+          '/pt/guides/',
+          '/pt/api/',
+          '/pt/api/wrapper/',
+          '/pt/api/wrapper-array/',
+          '/pt/api/options',
+          '/pt/api/components/'
+        ]
+      },
     }
   },
   extendMarkdown: md => {
